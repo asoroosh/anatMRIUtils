1b788b61562b543b25d6cb59ea1a75c498b3d326
asoroosh <srafyouni@gmail.com>
2019-07-11 15:57:14 +0100

    FS dir and also update on field trip

--- EXE/SPM/ft_example_segrun.m
+++ EXE/SPM/ft_example_segrun.m
@@ -3,9 +3,9 @@ addpath('/Users/sorooshafyouni/Home/matlab/fieldtrip')
 ft_defaults
 mri  = ft_read_mri('/Users/sorooshafyouni/Desktop/HCP/100307/unprocessed/3T/T1w_MPR1/100307_3T_T1w_MPR1.nii.gz');
 % use the ft_volumereslice function to be able to plot the MRI with the top of the head upwards
-cfg              = [];
-cfg.dim          = [256 256 256];                 % original dimension
-mri              = ft_volumereslice(cfg,mri);
+% cfg              = [];
+% cfg.dim          = [256 256 256];                 % original dimension
+% mri              = ft_volumereslice(cfg,mri);
 mri.coordsys     = 'spm';
 
 % obtain the brain, skull and scalp tissues
