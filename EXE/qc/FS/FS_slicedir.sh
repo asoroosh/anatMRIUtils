68b89eca50a4ff4b23d9d9aa6aa23b3c2d001535
asoroosh <srafyouni@gmail.com>
2019-07-16 09:55:44 +0000

    QC clean up

---
+++ EXE/qc/FS/FS_slicedir.sh
@@ -0,0 +1,4 @@
+
+
+
+${FSLDIR}/bin/slicesdir 
---
+++ EXE/qc/FSL/Submit_QC_linreg_T13D.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --partition=main
+#SBATCH --job-name=QC_linreg_T13D
+#SBATCH --mem=5G
+#SBATCH --time=20:00
+#SBATCH --output=/home/bdivdi.local/dfgtyk/NVROXBOX/EXE/qc/logs/QC_linreg_T13D.out
+#SBATCH --error=/home/bdivdi.local/dfgtyk/NVROXBOX/EXE/qc/logs/QC_linreg_T13D.err
+
+## Code goes here ## ## ##
+
+sh /home/bdivdi.local/dfgtyk/NVROXBOX/EXE/qc/NVR-OX-SLICESDIR-QC.sh CFTY720D2201E2 T13D linreg
+
+## ## ## ## ## ## ## ## ## ## ## ##
+
