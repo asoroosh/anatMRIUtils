3597d2b553d96cc599573ba73eab632c30ff63e8
asoroosh <srafyouni@gmail.com>
2019-08-02 12:55:29 +0000

    cat12 to be tested

---
+++ EXE/CAT12/NVR-OX-CAT12-ALLSTUDY.sh
@@ -0,0 +1,22 @@
+
+StudyIDTxtFileName=${HOME}/NVROXBOX/EXE/getinfo/StudyIDs_Clean.txt
+NUMJB=100
+
+# Loop around the image types
+for ImgTyp in T12D
+do
+	#Loop around the study IDs
+	while read StudyID
+	do
+
+		echo "=============================================================="
+                echo "=============================================================="
+                echo ""
+		echo "*** ${StudyID}, ${ImgTyp}"
+		echo ""
+                echo "=============================================================="
+                echo "=============================================================="
+		sh NVR-OX-CAT12.sh ${StudyID} ${ImgTyp} ${NUMJB}
+
+	done<$StudyIDTxtFileName
+done
