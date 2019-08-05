7229f20f758873027215d01c289a352709183d0f
asoroosh <srafyouni@gmail.com>
2019-08-05 08:21:16 +0000

    check job status if failed prints out the reason and nodelist

---
+++ EXE/jobcheck/NVR-OX-JOBCHECK-NODELIST.sh
@@ -0,0 +1,67 @@
+# Check the jobs and possibly resubmit them
+
+DirSuffix=autorecon12
+ImgTyp=T12D # Here we only use T13D and T12D
+
+NUMJB_INPT=200
+
+DataDir="${HOME}/NVROXBOX/Data"
+Path2StudyTxtFile=${HOME}/NVROXBOX/EXE/getinfo/StudyIDs_Clean.txt
+
+#This should later be in a loop around StudyIDs
+while read StudyID
+do
+	StudyDir="${DataDir}/${StudyID}"
+	ImgTypDir=${StudyDir}/${ImgTyp}
+	ImageFileTxt=${ImgTypDir}/${StudyID}_${ImgTyp}_ImageList.txt
+
+	NUMJB=$NUMJB_INPT
+	NUMJB_SUCCESSFUL=
+
+	if [ -z $NUMJB ]
+	then
+		NUMJB=$(cat $ImageFileTxt | wc -l)
+	else
+		NUMJB_tmp=$(cat $ImageFileTxt | wc -l)
+		NUMJBList_tmp=($NUMJB $NUMJB_tmp)
+		IFS=$'\n'
+		NUMJB=$(echo "${NUMJBList_tmp[*]}" | sort -n | head -n1)
+	fi
+
+	WhereTheStatFilesAre=${HOME}/NVROXBOX/Data/${StudyID}/${ImgTyp}/${DirSuffix}/Logs_02-08-19
+
+#	echo ${WhereTheStatFilesAre}
+	Path2StatFile=${WhereTheStatFilesAre}/${StudyID}_${DirSuffix}_${NUMJB}_*_*.stat
+#	echo ${Path2StatFile}
+	NUMJB_SUCCESSFUL=$(cat ${Path2StatFile} | awk '{print $2}' | awk '{s+=$1} END {print s}')
+
+	echo "======= ====== ====== ====== ======"
+	echo "=+_+_+_+_+_+_+_ ${StudyID} _+_+_+_+_+_+_+_+_+"
+	echo "Number of successulf jobs: ${NUMJB_SUCCESSFUL}"
+	echo "Number of expected jobs: ${NUMJB}"
+	echo "======= ====== ====== ====== ======"
+
+	rm -f ${StudyID}_${DirSuffix}.failedjobs.txt
+
+	for i_NUMJB in `seq 1 ${NUMJB}`
+	do
+		Path2StatPerFile=${WhereTheStatFilesAre}/${StudyID}_${DirSuffix}_${NUMJB}_*_${i_NUMJB}.stat
+		JBSTAT=$(cat ${Path2StatPerFile} | awk '{print $2}')
+
+		if [ ${JBSTAT} == 0 ]; then
+#			echo ${Path2StatPerFile}
+
+			FileName=$(basename $Path2StatPerFile .stat)
+			JOBID="$(cut -d'_' -f4 <<<"$FileName")"
+
+			echo "Stat:${JBSTAT}, JobNumb:${i_NUMJB}, JobID:${JOBID}" >> ${StudyID}_${DirSuffix}.failedjobs.txt
+			cat ${WhereTheStatFilesAre}/${StudyID}_${DirSuffix}_${NUMJB}_${JOBID}_${i_NUMJB}.err >> ${StudyID}_${DirSuffix}.failedjobs.txt
+			sacct --format=Nodelist -j ${JOBID}_${i_NUMJB} >> ${StudyID}_${DirSuffix}.failedjobs.txt
+
+
+		fi
+	done
+
+
+done<${Path2StudyTxtFile}
+
