ba43838b52510bf85441b672b0fa17cd07d229cb
asoroosh <srafyouni@gmail.com>
2020-01-21 15:41:33 +0000

    setdiff1d sorts the results, fixed now

--- SOURCE/mis/datepace.py
+++ SOURCE/mis/datepace.py
@@ -1,7 +1,7 @@
 #find the scans closesest to the 5 evenly paced milestones. 
 # Example:
-#[dfgtyk@bdivdi.local@frontend001 mis]$ ml Python 
-#[dfgtyk@bdivdi.local@frontend001 mis]$ python3 datepace.py ses-V10x20040918  ses-V17x20060309  ses-V1jM1x20150207  ses-V27x20090228  ses-V31x20100318  ses-V6x20040515    ses-V7x20040610  ses-V9x20040819 ses-V15x20050307  ses-V19x20070308  ses-V23x20080310    ses-V2x20040304   ses-V5x20040417   ses-V778x20110217  ses-V8x20040715
+#ml Python 
+#python3 datepace.py ses-V10x20040918  ses-V17x20060309  ses-V1jM1x20150207  ses-V27x20090228  ses-V31x20100318  ses-V6x20040515    ses-V7x20040610  ses-V9x20040819 ses-V15x20050307  ses-V19x20070308  ses-V23x20080310    ses-V2x20040304   ses-V5x20040417   ses-V778x20110217  ses-V8x20040715
 # ses-V2x20040304 ses-V19x20070308 ses-V27x20090228 ses-V778x20110217 ses-V1jM1x20150207 (this is list of session we will be sending to nonlinear SST)
 # ses-V10x20040417 ses-V15x20040515 ses-V17x20040610 ses-V23x20040715 ses-V31x20040819 ses-V5x20040918 ses-V6x20050307 ses-V7x20060309 ses-V8x20080310 ses-V9x20100318 (these are what we will be registrating to the SST later)
 #
@@ -62,19 +62,21 @@ def main():
          DD[i,j]=abs(msteps[i]-ts_v0[j])
  
  idx_ts=np.argmin(DD,axis=1)
+## send these to the SST
  ts_5dates=timestamps[idx_ts]
  sesaffix5d=sesaffix[idx_ts]
-
- sesaffixnot=np.setdiff1d(sesaffix,sesaffix5d)
- ts_no=np.setdiff1d(timestamps,ts_5dates)
- 
- VxNo=[]
- for ii in range(len(sesaffixnot)):
-     VxNo.append(sesaffixnot[ii]+'x'+str(int(ts_no[ii])))
 
  Vx5dates=[]
  for jj in range(len(sesaffix5d)):
      Vx5dates.append(sesaffix5d[jj]+'x'+str(int(ts_5dates[jj])))
+
+## send these to the reg2sst
+ ts_no=timestamps[~np.in1d(timestamps,ts_5dates)]
+ sesaffixnot=sesaffix[~np.in1d(sesaffix,sesaffix5d)]
+
+ VxNo=[]
+ for ii in range(len(sesaffixnot)):
+     VxNo.append(sesaffixnot[ii]+'x'+str(int(ts_no[ii])))
  
  ## Spit out
  print(*Vx5dates)
