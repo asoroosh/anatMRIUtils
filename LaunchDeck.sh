fe5233871e6af0d8ff3e9cd25b91105ece044c57
asoroosh <srafyouni@gmail.com>
2020-08-17 18:22:30 +0100

    LauchDeck

---
+++ LaunchDeck.sh
@@ -0,0 +1,4 @@
+
+# Export this variable. So all function have access to this variable
+export MyOX_AUX_CONFIGFILE_PATH="${HOME}/NVROXBOX/AUX/Config/my-ox-path-setup-fromrescomp.myoxconfig"
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# NVROXBOX
+# MRIUtils
+
+Collection of scattered functions and scripts to analyse T1-weighted images for longitudinal analysis with FSL, ANTs, FreeSurfer. 
